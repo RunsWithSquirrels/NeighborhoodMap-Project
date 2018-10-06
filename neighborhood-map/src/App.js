3108a39c9123133ca6d997849dc4879774d61f9a
Mallory Tompsett <malloryfr9@gmail.com>
2018-10-05 20:46:22 -0500

    feat/docs: Added service worker functionality
    Also added references in comment in App.js.

--- neighborhood-map/src/App.js
+++ neighborhood-map/src/App.js
@@ -26,3 +26,8 @@ class App extends Component {
 }
 
 export default App;
+
+/* 
+* Special thanks to Ryan Waite for his assitance with this project (https://www.youtube.com/watch?v=LvQe7xrUh7I&t=3837s&index=6&list=PLKC17wty6rS1XVZbRlWjYU0WVsIoJyO3s - Accessed October 5th, 2018; 
+* https://github.com/ryanwaite28/udacity-fend-p7 - Accessed October 5th, 2018) as well as special thanks to Forrest Walker for his assitance with this project (https://www.youtube.com/playlist?list=PL4rQq4MQP1crXuPtruu_eijgOUUXhcUCP - Accessed October 5th, 2018).
+*/
--- neighborhood-map/src/serviceWorker.js
+++ neighborhood-map/src/serviceWorker.js
@@ -125,3 +125,16 @@ export function unregister() {
     });
   }
 }
+
+//  Code for initalizing/starting service worker.
+export default function startSW() {
+  if ('serviceWoker' in navigator) {
+    navigator.serviceWorker.register('/sw.js', {scope: '/'})
+    .then(function(reg) {
+      console.log('Service worker registration worked!', reg);
+    })
+    .catch(function(error) {
+      console.log('Service worker registration failed with the following error:' + error);
+    });
+  }
+}
