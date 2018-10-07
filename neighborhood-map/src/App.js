887b33e433720a0dccb7cb5dda0b8a64c95b5d63
Mallory Tompsett <malloryfr9@gmail.com>
2018-10-06 22:31:09 -0500

    feat: Added markers to map

--- neighborhood-map/src/App.js
+++ neighborhood-map/src/App.js
@@ -4,18 +4,40 @@ import Map from './components/Map';
 import FourAPI from './API/';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      venues: [],
+      markers: [],
+      center: [],
+      zoom: 13
+    }
+  }
   componentDidMount() {
     FourAPI.search({
       near: "Austin, TX",
       query: "tacos",
       limit: 10 
-    }).then(results => console.log(results));
+    }).then(results => {
+      const { venues } = results.response;
+      const { center} = results.response.geocode.feature.geometry;
+      const markers = venues.map(venue => {
+        return {
+          lat: venue.location.lat,
+          lng: venue.location.lng,
+          isOpen: false,
+          isSeen: true,
+        }
+      });
+      this.setState({ venues, center, markers });
+      console.log(results);
+    });
   }
   render() {
     return (
       <div className="App">
         <div role="application" aria-hidden="true" id="map">
-          <Map />
+          <Map {...this.state}/>
         </div>
       </div>
     )
@@ -26,5 +48,6 @@ export default App;
 
 /* 
 * Special thanks to Ryan Waite for his assitance with this project (https://www.youtube.com/watch?v=LvQe7xrUh7I&t=3837s&index=6&list=PLKC17wty6rS1XVZbRlWjYU0WVsIoJyO3s - Accessed October 5th, 2018; 
-* https://github.com/ryanwaite28/udacity-fend-p7 - Accessed October 5th, 2018) as well as special thanks to Forrest Walker for his assitance with this project (https://www.youtube.com/playlist?list=PL4rQq4MQP1crXuPtruu_eijgOUUXhcUCP - Accessed October 5th, 2018).
+* https://github.com/ryanwaite28/udacity-fend-p7 - Accessed October 5th, 2018) as well as special thanks to Forrest Walker for his assitance with this project 
+* (https://www.youtube.com/playlist?list=PL4rQq4MQP1crXuPtruu_eijgOUUXhcUCP - Accessed October 5th, 2018).
 */
--- neighborhood-map/src/components/Map.js
+++ neighborhood-map/src/components/Map.js
@@ -1,17 +1,29 @@
 import React, { Component } from 'react';
 import { withScriptjs, withGoogleMap, GoogleMap, Marker } from "react-google-maps";
 
-const MyMapComponent = withScriptjs(withGoogleMap((props) =>
-    <GoogleMap defaultZoom={8} defaultCenter={{ lat: -34.397, lng: 150.644 }}>
-        {props.isMarkerShown && <Marker position={{ lat: -34.397, lng: 150.644 }} />}
-    </GoogleMap>
-))
+const MyMapComponent = withScriptjs(
+    withGoogleMap(props => (
+    <GoogleMap 
+    defaultZoom={8} 
+    zoom={props.zoom} 
+    defaultCenter={{ lat: -34.397, lng: 150.644 }}
+    center={props.center}
+    >
+    {props.markers && 
+    props.markers
+    .filter(marker => marker.isSeen)
+    .map((marker, indx) => 
+    <Marker key={indx} position={{ lat: marker.lat, lng: marker.lng }} />
+    )}
+        </GoogleMap>
+    ))
+);
 
 class Map extends Component {
     render() {
         return (
             <MyMapComponent
-            isMarkerShown
+            {...this.props}
             googleMapURL= "https://maps.googleapis.com/maps/api/js?v=3.exp&key=AIzaSyC2cqf79ch5dp8cXoXHyYSvFptiaVH7Tps"
             loadingElement = {< div style = {{ height: `100%` }} />}
             containerElement = {< div style = {{ height: `400px` }} />}
