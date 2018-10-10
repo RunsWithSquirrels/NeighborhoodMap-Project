b3fc287f0458e69aba3bb0ac0eabc5d6cce4a063
Mallory Tompsett <malloryfr9@gmail.com>
2018-10-10 15:42:29 -0500

    fix: Added more css for responsiveness

--- neighborhood-map/src/components/VenueItem.js
+++ neighborhood-map/src/components/VenueItem.js
@@ -4,7 +4,7 @@ class VenueItem extends Component {
     render() {
         return(
             <li className="venueItem" onClick={() => this.props.handleItemAction(this.props)}>
-                <img src={this.props.categories[0].icon.prefix+"32"+this.props.categories[0].icon.suffix} 
+                <img className="icon" src={this.props.categories[0].icon.prefix+"32"+this.props.categories[0].icon.suffix} 
                 alt={this.props.categories[0].name}/>
                 {this.props.name}
             </li>
