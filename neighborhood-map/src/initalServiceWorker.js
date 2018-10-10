27df89252004348e0ce234d97ba82ad8b466b7c3
Mallory Tompsett <malloryfr9@gmail.com>
2018-10-10 13:11:47 -0500

    fix/feat: Trying to get Service Worker working

---
+++ neighborhood-map/src/initalServiceWorker.js
@@ -0,0 +1,12 @@
+//  Code for initalizing/starting service worker.
+export default function initalServiceWorker() {
+  if ('serviceWoker' in navigator) {
+    navigator.serviceWorker.register('/sw.js', { scope: '/' })
+      .then(function (reg) {
+        console.log('Service worker registration worked!', reg);
+      })
+      .catch(function (error) {
+        console.log('Service worker registration failed with the following error:' + error);
+      });
+  }
+}
