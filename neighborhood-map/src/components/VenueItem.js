292af3cb2cfef156697254867d8519215632d967
Mallory Tompsett <malloryfr9@gmail.com>
2018-10-15 11:44:41 -0500

    fix: Working on adding ARIA roles

--- neighborhood-map/src/components/VenueItem.js
+++ neighborhood-map/src/components/VenueItem.js
@@ -4,9 +4,9 @@ class VenueItem extends Component {
     render() {
         return(
             <li className="venueItem" onClick={() => this.props.handleItemAction(this.props)}>
-                <img className="icon" src={this.props.categories[0].icon.prefix+"32"+this.props.categories[0].icon.suffix} 
-                alt={this.props.categories[0].name}/>
-                {this.props.name}
+                    <img className="icon" src={this.props.categories[0].icon.prefix+"32"+this.props.categories[0].icon.suffix} 
+                    alt={this.props.categories[0].name}/>
+                    {this.props.name}
             </li>
         );
     }
