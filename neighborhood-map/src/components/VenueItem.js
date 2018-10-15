54154f679a47c5a69562a531abbffd7bc636bdf4
Mallory Tompsett <malloryfr9@gmail.com>
2018-10-15 11:46:51 -0500

    fix: Added an id

--- neighborhood-map/src/components/VenueItem.js
+++ neighborhood-map/src/components/VenueItem.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class VenueItem extends Component {
     render() {
         return(
-            <li className="venueItem" onClick={() => this.props.handleItemAction(this.props)}>
+            <li className="venueItem" id="venueItem" onClick={() => this.props.handleItemAction(this.props)}>
                     <img className="icon" src={this.props.categories[0].icon.prefix+"32"+this.props.categories[0].icon.suffix} 
                     alt={this.props.categories[0].name}/>
                     {this.props.name}
